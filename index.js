56f0cc49837f15626e3f037ba743279d83fb16f3
Afanasii Kurakin <afanasy@gmail.com>
2016-04-21 18:45:48 +0800

    renamed ctx.sql to ctx.query, 1.0.1

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var _ = require('underscore')
-var stream = require('stream')
 var mysql = require('mysql')
 
 module.exports = function (d) {
@@ -7,25 +6,13 @@ module.exports = function (d) {
   return function (ctx, next) {
     if (!db)
       db = mysql.createPool(d)
-    if (ctx.queryString == 'dbEnd')
+    if (db && (ctx.queryString == 'dbEnd'))
       return db.end(ctx.done)
-    if (!_.isFunction(ctx.done)) {
-      return db.query(ctx.queryString).stream().pipe(new stream.Transform({
-        objectMode: true,
-        transform: function (data, encoding, next) {
-          //ctx = _.clone(ctx)
-          //ctx.data = data
-          //ctx.done = function (err, data) {
-            this.push(data)
-            next()
-          //}.bind(this)
-          //self.pump(ctx)
-        }
-      }))
-    }
-    db.query(ctx.queryString, function (err, data, fields) {
+    if (!_.isFunction(ctx.done))
+      return db.query(ctx.queryString).stream()
+    db.query(ctx.queryString, function (err, data, field) {
       ctx.data = data
-      ctx.fields = fields
+      ctx.field = field
       next(err)
     })
   }
